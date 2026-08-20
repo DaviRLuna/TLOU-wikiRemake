761835a518c23bce7408289d0719102aafca5c6d
DAVI RODRIGUES DE LUNA <davi.luna@aluno.cps.sp.gov.br>
2026-08-20 09:06:30 -0300

    FUNCIONOU

--- src/js/estrutura.js
+++ src/js/estrutura.js
@@ -5,32 +5,34 @@ const Audio = document.getElementById("meuAudio");
 
 CorpoPagina.classList.add("class-escondida");
 
-Entrar.addEventListener('click', function(){
-    BoasVindas.classList.add("class-escondida");
-    CorpoPagina.classList.remove("class-escondida");
-    Audio.play();
-
-    start()
-});
-
 let time = 2000;
-let pastImageIndex = currentImageIndex - 1;
 let currentImageIndex = 0;
-
 const img = document.querySelectorAll("#img img");
 max = img.length;
 
-function nextImage(){
+function nextImage() {
+    if(currentImageIndex === max) {
+        currentImageIndex = 0;
+        img[currentImageIndex].classList.add("visible");}
+    img[currentImageIndex].classList.remove("visible");
     currentImageIndex++;
     console.log(currentImageIndex);
-    img[currentImageIndex].classList.add("visible")
-
-    img[pastImageIndex].classList.remove("visible")
+    img[currentImageIndex].classList.add("visible");
 }
+
+
 
 function start(){
-    setInterval(() =>{
-        nextImage()
-    },time)
+    setInterval(() => {
+        nextImage();
+    }, time);
 }
+
+Entrar.addEventListener('click', function(){
+    console.log('entrou');
+    BoasVindas.classList.add("class-escondida");
+    CorpoPagina.classList.remove("class-escondida");
+    Audio.play();
+    start();
+});
 
