9d4705a2866718f37c7757e05c66d9e2c7cbd5b5
DAVI RODRIGUES DE LUNA <davi.luna@aluno.cps.sp.gov.br>
2026-09-04 09:05:36 -0300

    prototipo do prototipo do prototipo da sabor pagina

--- src/js/estruturaPerso.js
+++ src/js/estruturaPerso.js
@@ -1,12 +1,15 @@
 const cartao = document.getElementById("card");
+const div = document.getElementById("caixa");
+const Texto = document.getElementById("texto");
 
 cartao.addEventListener('click', function() {
     console.log('click')
     if(cartao.classList.contains('cards-perso-extendido')) {
         cartao.classList.remove("cards-perso-extendido");
-        console.log('voltou')
+        Texto.textContent = "";
     } else {
         cartao.classList.add("cards-perso-extendido");
-        console.log('foi')
+        Texto.textContent = "USHUAIGDVDSFISAJFVHDSBKJFDSBHVSDHGDSKFLKJDGBKJDS";
     }
 })
+
