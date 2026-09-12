c7055585ef5f438998b8c54178cd2828024aee30
DaviRLuna <dluninha11@gmail.com>
2026-09-12 13:38:17 -0300

    protótipo da exp. de tela pronto

--- src/js/estruturaPerso.js
+++ src/js/estruturaPerso.js
@@ -1,8 +1,30 @@
-const cartao = document.getElementById("card");
+const cartoes = document.querySelectorAll(".card");
+
+cartoes.forEach(cartao => {
+    cartao.addEventListener('click', function() {
+        const Expandido = cartao.classList.contains('extendido-tela');
+
+        cartoes.forEach(c => {
+            c.classList.remove('extendido-tela');
+            c.classList.remove('escondido');
+            c.querySelector('.texto').textContent = "";
+        });
+        if (!Expandido) {
+            cartao.classList.add('extendido-tela');
+            cartao.querySelector('.texto').textContent = cartao.dataset.texto;
+
+            cartoes.forEach(c => {
+                if (c !== cartao) {
+                    c.classList.add('escondido');
+                }
+            });
+        }
+    })
+})
 const div = document.getElementById("caixa");
 const Texto = document.getElementById("texto");
 
-cartao.addEventListener('click', function() {
+/*cartao.addEventListener('click', function() {
     console.log('click')
     if(cartao.classList.contains('cards-perso-extendido')) {
         cartao.classList.remove("cards-perso-extendido");
@@ -11,5 +33,5 @@ cartao.addEventListener('click', function() {
         cartao.classList.add("cards-perso-extendido");
         Texto.textContent = "USHUAIGDVDSFISAJFVHDSBKJFDSBHVSDHGDSKFLKJDGBKJDS";
     }
-})
+})*/
 
